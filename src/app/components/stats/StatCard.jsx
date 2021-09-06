326e8afc0969716b9c59741860fe7e1930cab3f9
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-09-06 12:52:40 +0530

    add status card

--- src/app/components/stats/StatCard.jsx
+++ src/app/components/stats/StatCard.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import {
+	Box,
+	Heading,
 	Stat,
+	StatGroup,
 	StatLabel,
 	StatNumber,
 	StatHelpText,
 	StatArrow,
 } from '@chakra-ui/react';
 
-const StatCard = (props) => {
+const StatCard = ({ heading }) => {
 	return (
-		<Stat>
-			<StatLabel>Sent</StatLabel>
-			<StatNumber>345,670</StatNumber>
-			<StatHelpText>
-				<StatArrow type='increase' />
-				23.36%
-			</StatHelpText>
-		</Stat>
+		<Box p={4}>
+			{/* Heading */}
+			<Heading as='h4' size='md' py={3}>
+				{heading}
+			</Heading>
+			<StatGroup>
+				<Stat>
+					<StatLabel>This Month</StatLabel>
+					<StatNumber>245,600</StatNumber>
+					<StatHelpText>
+						<StatArrow type='increase' />
+						23.36%
+					</StatHelpText>
+				</Stat>
+				<Stat>
+					<StatLabel>Last Month</StatLabel>
+					<StatNumber>345,670</StatNumber>
+				</Stat>
+				<Stat display={{ base: 'none', xl: 'block' }}>
+					<StatLabel>Previous Month</StatLabel>
+					<StatNumber>345,670</StatNumber>
+				</Stat>
+			</StatGroup>
+		</Box>
 	);
 };
 
