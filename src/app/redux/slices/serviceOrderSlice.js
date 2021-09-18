bc370e53e0c996dc8c448c416dbae44b0e7917ab
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-09-18 20:17:28 +0530

    add serviceOrderSlice

---
+++ src/app/redux/slices/serviceOrderSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getServiceOrdersAsync = createAsyncThunk(
+	'serviceOrders/getServiceOrdersAsync',
+	async () => {
+		const resp = await fetch(
+			'https://run.mocky.io/v3/081cc275-f318-4292-9abd-738943342ec6'
+		);
+		if (resp.ok) {
+			const response = await resp.json();
+			const serviceOrders = response.data;
+			return { serviceOrders: serviceOrders };
+		}
+	}
+);
+
+export const serviceOrderSlice = createSlice({
+	name: 'serviceOrders',
+	initialState: [],
+	reducers: {},
+	extraReducers: {
+		[getServiceOrdersAsync.fulfilled]: (state, action) => {
+			return action.payload.serviceOrders;
+		},
+	},
+});
+
+export const {} = serviceOrderSlice.actions;
+
+export const selectAllServiceOrders = (state) => state.serviceOrders;
+
+export default serviceOrderSlice.reducer;
--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import salespersonReducer from './slices/salespersonSlice';
 import customerReducer from './slices/customerSlice';
+import serviceOrderReducer from './slices/serviceOrderSlice';
+import locationReducer from './slices/locationsSlice';
 
 export default configureStore({
 	reducer: {
 		salespersons: salespersonReducer,
 		customers: customerReducer,
+		serviceOrders: serviceOrderReducer,
+		locations: locationReducer,
 	},
 });
