54723980b250cae77d042363d363e8c98a446d12
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-09-18 07:06:48 +0530

    add customerSlice to store

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import salespersonReducer from './slices/salespersonSlice';
+import customerReducer from './slices/customerSlice';
 
 export default configureStore({
 	reducer: {
 		salespersons: salespersonReducer,
+		customers: customerReducer,
 	},
 });
