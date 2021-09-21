1e22c071c5ca9efc9d9252c44fb3a10ee4be93ee
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-09-21 06:18:01 +0530

    add login page

--- src/app/components/pages/login/LoginPage.jsx
+++ src/app/components/pages/login/LoginPage.jsx
@@ -1,120 +1,165 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-	Flex,
 	Box,
+	Button,
+	IconButton,
+	Checkbox,
 	FormControl,
 	FormLabel,
-	Input,
-	Checkbox,
-	Stack,
-	Link,
-	Button,
 	Heading,
-	FormHelperText,
-	useColorModeValue,
+	Input,
+	Stack,
+	Text,
+	Link,
+	Flex,
+	InputGroup,
+	InputRightElement,
+	FormErrorMessage,
 } from '@chakra-ui/react';
-
-// import { userActions } from '../../redux/actions';
+import { ArrowForwardIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import {
+	loginPending,
+	loginSuccess,
+	loginFail,
+} from '../../../redux/slices/loginSlice';
+import { getUserProfile } from '../../../redux/actions/userActions';
+import { userLogin } from '../../../../api/userApi';
 
 const LoginPage = (props) => {
-	const { loggingIn } = props;
-	const [state, changeState] = useState({
-		email: '',
-		password: '',
-		submitted: false,
-	});
-	const handleChange = (value) => {
-		changeState({ ...state, ...value });
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-
-		changeState(...state, { submitted: true });
-		if (state.email && state.password) {
-			props.login(state.email, state.password);
-		}
-	};
+	const dispatch = useDispatch();
+	const history = useHistory();
+	let location = useLocation();
+	const [passwordShow, setPasswordShow] = useState(false);
+	const { isLoading, isAuth, error } = useSelector((state) => state.login);
+	let { from } = location.state || { from: { pathname: '/' } };
+	useEffect(() => {
+		sessionStorage.getItem('accessJWT') && history.replace(from);
+	}, [history, isAuth, from]);
+	const handlePasswordShow = () => setPasswordShow(!passwordShow);
 	return (
-		<Flex
-			minH={'100vh'}
-			align={'center'}
-			justify={'center'}
-			bg={useColorModeValue('gray.50', 'gray.800')}
-		>
-			<Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-				<Stack align={'center'}>
-					<Heading fontSize={'4xl'}>Sign in to your account</Heading>
-				</Stack>
-				<Box
-					rounded={'lg'}
-					bg={useColorModeValue('white', 'gray.700')}
-					boxShadow={'lg'}
-					p={8}
-				>
-					<Stack spacing={4}>
-						<FormControl id='email'>
-							<FormLabel>Email address</FormLabel>
-							<Input
-								type='email'
-								value={state.email}
-								onChange={(e) => handleChange({ email: e.target.value })}
-							/>
-							{state.submitted && !state.email && (
-								<FormHelperText>Username is required</FormHelperText>
-							)}
-						</FormControl>
-						<FormControl id='password'>
-							<FormLabel>Password</FormLabel>
-							<Input
-								type='password'
-								value={state.password}
-								onChange={(e) => handleChange({ password: e.target.value })}
-							/>
-							{state.submitted && !state.password && (
-								<FormHelperText>Password is required</FormHelperText>
-							)}
-						</FormControl>
-						<Stack spacing={10}>
-							<Stack
-								direction={{ base: 'column', sm: 'row' }}
-								align={'start'}
-								justify={'space-between'}
-							>
-								<Checkbox>Remember me</Checkbox>
-								<Link color={'blue.400'}>Forgot password?</Link>
-							</Stack>
-							<Button
-								bg={'blue.400'}
-								color={'white'}
-								onClick={handleSubmit}
-								isLoading={loggingIn}
-								_hover={{
-									bg: 'blue.500',
-								}}
-							>
-								Sign in
-							</Button>
-						</Stack>
-					</Stack>
+		<Flex minHeight='100vh' minW='100vw' align='center' justify='center'>
+			<Box
+				px={8}
+				py={4}
+				maxWidth='600px'
+				borderRadius={10}
+				width='full'
+				bg='transparent'
+			>
+				<Box textAlign='center'>
+					<Heading>Sign in to Your Account</Heading>
 				</Box>
-			</Stack>
+				<Box my={8} textAlign='center'>
+					<Formik
+						initialValues={{
+							email: '',
+							password: '',
+						}}
+						validationSchema={Yup.object({
+							email: Yup.string()
+								.max(100)
+								.email('Invalid email')
+								.required('Email cannot be empty'),
+							password: Yup.string().required('Password cannot be empty'),
+						})}
+						onSubmit={async (values) => {
+							dispatch(loginPending());
+							try {
+								const isAuth = await userLogin(values);
+
+								if (isAuth.status === 'error') {
+									return dispatch(loginFail(isAuth.message));
+								}
+
+								dispatch(loginSuccess());
+								dispatch(getUserProfile());
+								history.push('/');
+							} catch (error) {
+								dispatch(loginFail(error.message));
+							}
+						}}
+					>
+						{(props) => (
+							<Box>
+								{error && (
+									<Text fontSize='16px' color='tomato'>
+										{error}
+									</Text>
+								)}
+
+								<FormControl
+									isInvalid={props.errors.email && props.touched.email}
+								>
+									<FormLabel>Email Address:</FormLabel>
+									<Input
+										type='email'
+										placeholder='Enter your Email Address'
+										name='email'
+										value={props.initialValues.email}
+										{...props.getFieldProps('email')}
+									/>
+									<FormErrorMessage>{props.errors.email}</FormErrorMessage>
+								</FormControl>
+								<FormControl
+									isInvalid={props.errors.password && props.touched.password}
+								>
+									<FormLabel>Password:</FormLabel>
+									<InputGroup>
+										<Input
+											type={passwordShow ? 'text' : 'password'}
+											placeholder='Enter your Password'
+											name='password'
+											value={props.initialValues.password}
+											{...props.getFieldProps('password')}
+										/>
+										<InputRightElement width='4.5rem'>
+											<IconButton
+												variant='unstyled'
+												colorScheme='teal'
+												size='md'
+												aria-label={
+													passwordShow ? 'Hide Password' : 'Show Password'
+												}
+												onClick={handlePasswordShow}
+												icon={passwordShow ? <ViewOffIcon /> : <ViewIcon />}
+											/>
+										</InputRightElement>
+									</InputGroup>
+									<FormErrorMessage>{props.errors.password}</FormErrorMessage>
+								</FormControl>
+								<Stack isInline justifyContent='space-between' mt={4}>
+									<Box>
+										<Checkbox>Remember Me</Checkbox>
+									</Box>
+									<Box>
+										<Link color={`green.500`} href='#'>
+											Forget Your Password?
+										</Link>
+									</Box>
+								</Stack>
+								<Button
+									onClick={props.submitForm}
+									colorScheme='green'
+									rightIcon={<ArrowForwardIcon />}
+									width='full'
+									mt={4}
+									isLoading={isLoading}
+									loadingText='Signinig in'
+								>
+									Sign In
+								</Button>
+							</Box>
+						)}
+					</Formik>
+				</Box>
+			</Box>
 		</Flex>
 	);
 };
-
-// function mapState(state) {
-// 	const { loggingIn } = state.authentication;
-// 	return { loggingIn };
-// }
-
-// const actionCreators = {
-// 	login: userActions.login,
-// 	logout: userActions.logout,
-// };
-
-// const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
-// export { connectedLoginPage as LoginPage };
 
 export default LoginPage;
