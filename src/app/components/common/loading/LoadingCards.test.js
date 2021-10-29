bcbe81e086cc24d8917e4e847fd09c13cc74fad2
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-10-29 18:02:20 +0530

    add LoadingCards tests errors

---
+++ src/app/components/common/loading/LoadingCards.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import pretty from 'pretty';
+import LoadingCards from './LoadingCards';
+
+let container = null;
+beforeEach(() => {
+	// setup a DOM element as a render target
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+afterEach(() => {
+	// cleanup on exiting
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+describe('should render correctly', () => {
+	it('renders correctly when count has not passed as props', () => {
+		act(() => {
+			render(<LoadingCards />, container);
+		});
+		expect(pretty(container.innerHTML)).toMatchInlineSnapshot();
+	});
+	it('renders correctly when count passed', () => {
+		act(() => {
+			render(<LoadingCards count={4} />, container);
+		});
+		expect(pretty(container.innerHTML)).toMatchInlineSnapshot();
+	});
+});
